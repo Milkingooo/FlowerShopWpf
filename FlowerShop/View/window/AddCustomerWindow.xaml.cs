57704d201570b8d0a81894cac072d101a8bbdfed
Milkingooo <maks.trushin.06@bk.ru>
2025-03-26 19:28:22 +0300

    Добавьте файлы проекта.

---
+++ FlowerShop/View/window/AddCustomerWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using FlowerShop.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FlowerShop.View.window
+{
+    /// <summary>
+    /// Логика взаимодействия для AddCustomerWindow.xaml
+    /// </summary>
+    public partial class AddCustomerWindow : Window
+    {
+        public AddCustomerWindow()
+        {
+            InitializeComponent();
+
+            DataContext = new MainWindowViewModel();
+        }
+
+        private void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+            var result = (DataContext as MainWindowViewModel).AddCustomer();
+
+            if (result)
+            {
+                MessageBox.Show("Объект записан");
+                ((this.Owner as MainWindow).DataContext as MainWindowViewModel).LoadCustomers();
+                this.Close();
+            }
+        }
+    }
+}
---
+++ FlowerShop/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,108 @@
+﻿using FlowerShop.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace FlowerShop.ViewModel
+{
+    public class MainWindowViewModel : BVM
+    {
+        private string _name;
+        private string _phone;
+        private string _email;
+        private ObservableCollection<Customer> _customers;
+        private Customer _selectedCustomer;
+        private Customer _newCustomer;
+
+        public string Name
+        {
+            get => _name;
+            set => SetPropertyChanged(ref _name, value, nameof(Name));
+        }
+
+        public string Phone
+        {
+            get => _phone;
+            set => SetPropertyChanged(ref _phone, value, nameof(Phone));
+        }
+
+        public string Email
+        {
+            get => _email;
+            set => SetPropertyChanged(ref _email, value, nameof(Email));
+        }
+
+        public ObservableCollection<Customer> Customers
+        {
+            get => _customers;
+            set => SetPropertyChanged(ref _customers, value, nameof(Customers));
+        }
+
+        public Customer SelectedCustomer
+        {
+            get => _selectedCustomer;
+            set => SetPropertyChanged(ref _selectedCustomer, value, nameof(SelectedCustomer));
+        }
+
+        public Customer NewCustomer
+        {
+            get => _newCustomer;
+            set => SetPropertyChanged(ref _newCustomer, value, nameof(NewCustomer));
+        }
+
+        public MainWindowViewModel()
+        {
+            Customers = new ObservableCollection<Customer>();
+            NewCustomer = new Customer();
+        }
+
+        public void LoadCustomers()
+        {
+            Customers.Clear();
+            using(var context = new FlowerShopEntities())
+            {
+                var temp = context.Customer.ToList();
+
+                foreach (var customer in temp)
+                {
+                    Customers.Add(customer);
+                }
+            }
+        }
+
+        public void DeleteCustomer()
+        {
+            try
+            {
+                using(var context = new FlowerShopEntities())
+                {
+                    var findEntity = context.Customer.FirstOrDefault(s => s.Id == SelectedCustomer.Id);
+                    if (findEntity == null) return;
+                    var result = context.Customer.Remove(findEntity);
+                    context.SaveChanges();
+
+                    LoadCustomers();
+                }
+            }
+            catch
+            (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public bool AddCustomer()
+        {
+            using (var context = new FlowerShopEntities())
+            {
+                var newStudent = context.Customer.Add(NewCustomer);
+                context.SaveChanges();
+                return true;
+            }
+        }
+    }
+}
